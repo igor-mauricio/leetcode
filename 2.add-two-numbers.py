d344fc0a027c5d383e9c994ba8f347503a065d65
Igor Maurício <igor.mauricio.eng@gmail.com>
2024-09-28 02:13:19 -0300

    add two numbers ok

--- 2.add-two-numbers.py
+++ 2.add-two-numbers.py
@@ -15,25 +15,38 @@ from typing import Optional
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        sum_l1 = 0
-        while l1.next != None:
-            sum_l1 *= 10
-            sum_l1 += l1.val
-            l1 = l1.next
-        sum_l2 = 0
-        while l2.next != None:
-            sum_l2 *= 10
-            sum_l2 += l2.val
-            l2 = l2.next
-        sum = sum_l1 + sum_l2
-        result = ListNode()
-        current_node = result
-        while sum > 0:    
-            current_node.val = int(sum % 10)
-            result.next = ListNode()
-            current_node = result.next
-            sum = (sum - sum%10)/ 10
+        result = ListNode((l1.val + l2.val) % 10)
+        current_result = result
+        remainder = int((l1.val + l2.val - (l1.val + l2.val) % 10)/10)
+        l1 = l1.next
+        l2 = l2.next
+        while True:
+            if l1 is None and l2 is None:
+                if remainder > 0:
+                    current_result.next = ListNode(remainder)
+                break
+            if l1 is None:
+                current_result.next = ListNode((l2.val + remainder)% 10)
+                remainder = int((l2.val + remainder - (l2.val + remainder) % 10)/10)
+            elif l2 is None:
+                current_result.next = ListNode((l1.val + remainder)% 10)
+                remainder = int((l1.val + remainder - (l1.val + remainder) % 10)/10)
+            else:
+                current_result.next = ListNode((l1.val + l2.val + remainder) % 10)
+                remainder = int((l1.val + l2.val + remainder - (l1.val + l2.val + remainder) % 10)/10)
+            if l1 is not None:
+                l1 = l1.next
+            if l2 is not None:
+                l2 = l2.next
+            current_result = current_result.next
         return result
-        
+
+if __name__ == "__main__":
+    result = Solution().addTwoNumbers(ListNode(9, ListNode(9, ListNode(9, ListNode(9,ListNode(9,ListNode(9,ListNode(9))))))), ListNode(9, ListNode(9, ListNode(9,ListNode(9)))))
+    while True:
+        print(result.val)
+        if result.next == None:
+            break
+        result = result.next
 # @lc code=end
 
