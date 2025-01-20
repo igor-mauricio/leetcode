b27712fce8c068f8803a7da1fe38d8dbeaba69f5
Igor Maurício <igor.mauricio.eng@gmail.com>
2025-01-19 21:24:04 -0300

    14 - Longest common prefix

---
+++ 14.longest-common-prefix.go
@@ -0,0 +1,24 @@
+package leetcode
+
+/*
+ * @lc app=leetcode id=14 lang=golang
+ *
+ * [14] Longest Common Prefix
+ */
+
+// @lc code=start
+func longestCommonPrefix(strs []string) string {
+	output := ""
+	for i := range 200 {
+		for j := range strs {
+			if len(strs[j]) <= i || strs[j][i] != strs[0][i] {
+				return output
+			}
+		}
+		output = strs[0][0 : i+1]
+	}
+	return output
+
+}
+
+// @lc code=end
