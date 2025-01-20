cedb058c66bc168ef9597a6fd170ae7b4eb50bed
Igor Maurício <igor.mauricio.eng@gmail.com>
2025-01-19 21:33:34 -0300

    605 - Can place flowers solution

---
+++ 605.can-place-flowers.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=605 lang=java
+ *
+ * [605] Can Place Flowers
+ */
+
+// @lc code=start
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if (n == 0)
+            return true;
+
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0)
+                    && (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
+                flowerbed[i] = 1;
+                n--;
+                if (n <= 0)
+                    return true;
+            }
+        }
+        return n <= 0;
+    }
+}
+// @lc code=end
+
