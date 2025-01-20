5da9b930b2717bdecbcd5c2633901dfc4677f570
Igor Maurício <igor.mauricio.eng@gmail.com>
2025-01-19 21:24:15 -0300

    20 - valid parentheses

---
+++ 20.valid-parentheses.go
@@ -0,0 +1,40 @@
+package leetcode
+
+/*
+ * @lc app=leetcode id=20 lang=golang
+ *
+ * [20] Valid Parentheses
+ */
+
+// @lc code=start
+
+func isValid(s string) bool {
+	stack := []byte{}
+	for i := range s {
+		if len(stack) <= 0 {
+			stack = append(stack, s[i])
+			continue
+		}
+		switch s[i] {
+		case ']':
+			if stack[len(stack)-1] == '[' {
+				stack = stack[:len(stack)-1]
+				continue
+			}
+		case ')':
+			if stack[len(stack)-1] == '(' {
+				stack = stack[:len(stack)-1]
+				continue
+			}
+		case '}':
+			if stack[len(stack)-1] == '{' {
+				stack = stack[:len(stack)-1]
+				continue
+			}
+		}
+		stack = append(stack, s[i])
+	}
+	return len(stack) == 0
+}
+
+// @lc code=end
