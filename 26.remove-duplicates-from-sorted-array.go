963c33446fa14c722ec690d5c113cdfba8f48244
Igor Maurício <igor.mauricio.eng@gmail.com>
2025-01-19 21:24:38 -0300

    26 - remove duplicates from sorted array

---
+++ 26.remove-duplicates-from-sorted-array.go
@@ -0,0 +1,22 @@
+package leetcode
+
+/*
+ * @lc app=leetcode id=26 lang=golang
+ *
+ * [26] Remove Duplicates from Sorted Array
+ */
+
+// @lc code=start
+func removeDuplicates(nums []int) int {
+	index := 0
+	for i := range nums {
+		if nums[i] == nums[index] {
+			continue
+		}
+		index++
+		nums[index] = nums[i]
+	}
+	return index + 1
+}
+
+// @lc code=end
